35f070c3c64b9db210f13cb1e6ed6b8bb49ad6ee
error0x001 <71964062+error0x001@users.noreply.github.com>
2023-04-10 21:37:56 +0700

    Add arch (#2)
    * Add docker, nginx, compose, logic, periodic tasks, etc

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11
+
+# ENV DJANGO_SETTINGS_MODULE=moneybox.settings.production  TODO replace it as args
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY ./moneybox .
+
---
+++ moneybox/api/tasks.py
@@ -0,0 +1,18 @@
+from clients.currency.client import ExchangeRatesAPIClient, client
+
+
+def update_currency():
+    import django
+
+    django.setup()
+    from api.models import Currency, CurrencyRate
+
+    usd = Currency.get_usd()
+    currencies = client.get_currencies()
+    for code, rate in client.get_rates().items():
+        target_currency, _ = Currency.objects.get_or_create(
+            code=code, name=currencies[code]
+        )
+        CurrencyRate.objects.create(
+            source_currency=usd, target_currency=target_currency, rate=rate
+        )
