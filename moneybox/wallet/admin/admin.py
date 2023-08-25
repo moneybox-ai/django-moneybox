10411a815d317734b07fa8a788409a2bea2791f4
Егор Лоскутов <egorkaloskutov@yandex.ru>
2023-08-25 12:16:52 +0300

    Update admin.py

--- moneybox/wallet/admin/admin.py
+++ moneybox/wallet/admin/admin.py
@@ -1,21 +1,27 @@
 # vim: set fileencoding=utf-8 :
 from django.contrib import admin
-import wallet.models as models
-import users.models as users_models
+
+from users.models import Profile
 from wallet.admin import *
+from wallet.models.currency_models import Currency, CurrencyRate
+from wallet.models.expense_models import ExpenseCategory, Expense
+from wallet.models.group_models import Group
+from wallet.models.income_models import IncomeCategory, Income
+from wallet.models.transfer_models import Transfer
+from wallet.models.wallet_models import Wallet
 
 
 def _register(model, admin_class):
     admin.site.register(model, admin_class)
 
 
-_register(users_models.Profile, ProfileAdmin)
-_register(models.Group, GroupAdmin)
-_register(models.Wallet, WalletAdmin)
-_register(models.IncomeCategory, IncomeCategoryAdmin)
-_register(models.ExpenseCategory, ExpenseCategoryAdmin)
-_register(models.Income, IncomeAdmin)
-_register(models.Expense, ExpenseAdmin)
-_register(models.Transfer, TransferAdmin)
-_register(models.Currency, CurrencyAdmin)
-_register(models.CurrencyRate, CurrencyRateAdmin)
+_register(Profile, ProfileAdmin)
+_register(Group, GroupAdmin)
+_register(Wallet, WalletAdmin)
+_register(IncomeCategory, IncomeCategoryAdmin)
+_register(ExpenseCategory, ExpenseCategoryAdmin)
+_register(Income, IncomeAdmin)
+_register(Expense, ExpenseAdmin)
+_register(Transfer, TransferAdmin)
+_register(Currency, CurrencyAdmin)
+_register(CurrencyRate, CurrencyRateAdmin)
