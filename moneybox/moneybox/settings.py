69225110aaa1beb259b3100aa94ce30e3a94d3ab
error0x001 <71964062+error0x001@users.noreply.github.com>
2023-08-26 17:17:41 +0300

    Remove old models, admins, serializers. Fix migrations. (#13)

--- moneybox/moneybox/settings.py
+++ moneybox/moneybox/settings.py
@@ -69,9 +69,10 @@ WSGI_APPLICATION = "moneybox.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+LOCAL_ENV = "local"
 
-ENV = os.getenv("ENV", "local")
-if ENV == "local":
+ENV = os.getenv("ENV", LOCAL_ENV)
+if ENV == LOCAL_ENV:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
--- moneybox/wallet/admin/__init__.py
+++ moneybox/wallet/admin/__init__.py
@@ -1,11 +1,10 @@
-from .currency import CurrencyAdmin, CurrencyRateAdmin
-from .expense import ExpenseCategoryAdmin, ExpenseAdmin
-from .group import GroupAdmin
-from .income import IncomeCategoryAdmin, IncomeAdmin
-from .profile import ProfileAdmin
-from .transfer import TransferAdmin
-from .wallet import WalletAdmin
-
+from wallet.admin.currency import CurrencyAdmin, CurrencyRateAdmin
+from wallet.admin.expense import ExpenseAdmin, ExpenseCategoryAdmin
+from wallet.admin.group import GroupAdmin
+from wallet.admin.income import IncomeAdmin, IncomeCategoryAdmin
+from wallet.admin.profile import ProfileAdmin
+from wallet.admin.transfer import TransferAdmin
+from wallet.admin.wallet import WalletAdmin
 
 __all__ = [
     'CurrencyAdmin',
