0c61e43b7917661e6c439bf6ff961901364f8ee3
Артем Гронский <ares.crtc@gmail.com>
2023-08-28 14:41:52 +0300

    add /currency/pk/, make permission

--- moneybox/api/permissions.py
+++ moneybox/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    """Фllows unsafe methods to be performed only by administrators."""
+    """Allows unsafe methods to be performed only by administrators."""
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
