92209c427413f2ec2d09fefd6fd0b24f887e1ca4
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2023-08-30 22:49:00 +0300

    correcting signin view

--- moneybox/api/views/auth.py
+++ moneybox/api/views/auth.py
@@ -9,18 +9,15 @@ from users.models import CustomUser
 @api_view(("POST",))
 def signup(request):
     new_user = CustomUser.objects.create()
-    user_uuid = new_user.uuid
-    return Response({"uuid": user_uuid}, status=status.HTTP_201_CREATED)
+    return Response({"uuid": new_user.uuid}, status=status.HTTP_201_CREATED)
 
 
 @api_view(("POST",))
 def signin(request):
     serializer = CustomUserSerializer(data=request.data)
-    if serializer.is_valid():
-        token = serializer.validated_data['uuid']
-        if CustomUser.objects.filter(uuid=token).exists():
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response({"Authentication failed": "no such uuid exists"},
-                            status.HTTP_400_BAD_REQUEST)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer.is_valid(raise_exception=True)
+    token = serializer.validated_data['uuid']
+    if CustomUser.objects.filter(uuid=token).exists():
+        return Response(status=status.HTTP_200_OK)
+    return Response({"error": "no such uuid exists"},
+                    status.HTTP_401_UNAUTHORIZED)
