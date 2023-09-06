727109ed318569ac88382869d94e1c5fd38ca906
ArtemWhiter <ares.crtc@gmail.com>
2023-09-06 10:46:17 +0300

    delete custom scheduler

--- moneybox/moneybox/celery.py
+++ moneybox/moneybox/celery.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneybox.settings')
@@ -9,4 +8,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneybox.settings')
 app = Celery('moneybox')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
