5def6e7cac15b55f717be89e24521d83110fffba
hist <tsihaxam@gmail.com>
2023-09-10 00:19:56 +0500

    return admin init text

--- moneybox/wallet/admin/__init__.py
+++ moneybox/wallet/admin/__init__.py
@@ -1,20 +1,20 @@
-# from wallet.admin.currency import CurrencyAdmin, CurrencyRateAdmin
-# from wallet.admin.expense import ExpenseAdmin, ExpenseCategoryAdmin
-# from wallet.admin.group import GroupAdmin
-# from wallet.admin.income import IncomeAdmin, IncomeCategoryAdmin
-# from wallet.admin.profile import ProfileAdmin
-# from wallet.admin.transfer import TransferAdmin
-# from wallet.admin.wallet import WalletAdmin
-#
-# __all__ = [
-#     "CurrencyAdmin",
-#     "CurrencyRateAdmin",
-#     "ExpenseAdmin",
-#     "ExpenseCategoryAdmin",
-#     "GroupAdmin",
-#     "IncomeAdmin",
-#     "IncomeCategoryAdmin",
-#     "ProfileAdmin",
-#     "TransferAdmin",
-#     "WalletAdmin",
-# ]
+from wallet.admin.currency import CurrencyAdmin, CurrencyRateAdmin
+from wallet.admin.expense import ExpenseAdmin, ExpenseCategoryAdmin
+from wallet.admin.group import GroupAdmin
+from wallet.admin.income import IncomeAdmin, IncomeCategoryAdmin
+from wallet.admin.profile import ProfileAdmin
+from wallet.admin.transfer import TransferAdmin
+from wallet.admin.wallet import WalletAdmin
+
+__all__ = [
+    "CurrencyAdmin",
+    "CurrencyRateAdmin",
+    "ExpenseAdmin",
+    "ExpenseCategoryAdmin",
+    "GroupAdmin",
+    "IncomeAdmin",
+    "IncomeCategoryAdmin",
+    "ProfileAdmin",
+    "TransferAdmin",
+    "WalletAdmin",
+]
