f23fd419b8a4b8be28fabd11673e471e1eca166d
Egor Loskutov <119350657+Faithdev21@users.noreply.github.com>
2023-09-24 16:00:15 +0300

    Creating an invite code and adding user to the group (#34)

--- moneybox/moneybox/urls.py
+++ moneybox/moneybox/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from api.urls import urlpatterns as api_urlpatterns
 from core.urls import urlpatterns as core_urlpatterns
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"))]
 urlpatterns += api_urlpatterns
 urlpatterns += core_urlpatterns
