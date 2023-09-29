e9d6ec6166292eef7d69924ce053f4461897cc51
Stanislav Losev <losev_s_v@itgri.ru>
2023-09-29 10:44:56 +0300

    Celary deployment preparation

--- moneybox/wallet/admin/invite.py
+++ moneybox/wallet/admin/invite.py
@@ -5,7 +5,7 @@ from wallet.models.invite import Invite
 
 @admin.register(Invite)
 class InviteAdmin(admin.ModelAdmin):
-    list_display = ("id", "invite_code", "group" "created_at", "updated_at")
+    list_display = ("id", "invite_code", "group", "created_at", "updated_at")
     list_filter = ("created_at", "updated_at")
     search_fields = ("name", "group")
     date_hierarchy = "created_at"
