78ba5a3e58839f2af3b483bf5d4c0bb7fb6f4d78
Stanislav Losev <losev_s_v@itgri.ru>
2023-09-29 10:26:29 +0300

    Minor improvements of cbr client

--- moneybox/api/tasks.py
+++ moneybox/api/tasks.py
@@ -1,34 +1,26 @@
 from django.utils import timezone
 
-from clients.currency.cbrclient import cbr_сlient
+from clients.currency.cbrclient import cbr_client
 from moneybox.celery import app
 from wallet.models.currency import Currency, CurrencyRate
 from wallet.models.invite import Invite
 
 
-def create_rates(currency, rate):
-    CurrencyRate.objects.create(currency=currency, rate=rate)
-
-
-def create_or_update_table_currency(currencies):
+@app.task
+def get_exchange_rates():
+    """Delivering currency courses from cbr.ru"""
+    currencies = cbr_client.get_currencies_rates()
     for code, code_data in currencies.items():
-        Currency.objects.update_or_create(
+        currency, _ = Currency.objects.get_or_create(
             code=code,
             name=code_data.get("name", code),
         )
-        currency = (Currency.objects.get(code=code),)
         rate = code_data.get("value") / code_data.get("nominal")
-        create_rates(currency, rate)
-
-
-@app.task
-def get_exchange_rates():
-    """Delivering valute courses from cbr.ru."""
-    currencies = cbr_сlient.get_currencies_rates()
-    create_or_update_table_currency(currencies)
+        CurrencyRate.objects.create(currency=currency, rate=rate)
 
 
 @app.task
 def delete_expired_invites():
+    """Removes expired invites"""
     expired_invite_codes = Invite.objects.filter(expires_at__lt=timezone.now())
     expired_invite_codes.delete()
